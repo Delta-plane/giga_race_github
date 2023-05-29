329874384e36caded2e77cc0c7dfd9c8a9012d94
Blackmewto <131798330+Blackmewto@users.noreply.github.com>
2023-05-29 17:39:03 +0200

    Delete Limite_carre_central.py

--- Limite_carre_central.py
+++
@@ -1,6 +0,0 @@
-
-def est_dans_carre(Vaisseau,Carre):
-    if Carre.x<=Vaisseau.A[0]<=Carre.x+Carre.sizex:
-        if Carre.y<=Vaisseau.A[1]<=Carre.x+Carre.sizey:
-            print("est dedans")
-            
